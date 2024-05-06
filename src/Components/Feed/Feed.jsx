fe3bb8386844d5ede89d690b9fac421b5df868df
JesusVA760 <tonio4340406@gmail.com>
2024-05-05 23:25:45 -0700

    Sidebar

---
+++ src/Components/Feed/Feed.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Feed.css";
+import tumbnail1 from "../../assets/thumbnail1.png";
+import tumbnail2 from "../../assets/thumbnail2.png";
+import tumbnail3 from "../../assets/thumbnail3.png";
+import tumbnail4 from "../../assets/thumbnail4.png";
+import tumbnail5 from "../../assets/thumbnail5.png";
+import tumbnail6 from "../../assets/thumbnail6.png";
+import tumbnail7 from "../../assets/thumbnail7.png";
+import tumbnail8 from "../../assets/thumbnail8.png";
+
+const Feed = () => {
+  return <div className="card"></div>;
+};
+
+export default Feed;
