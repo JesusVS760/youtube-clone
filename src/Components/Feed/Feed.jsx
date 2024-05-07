2199c5b2904dc13ccfd385e243c8ee0102e8ae9c
JesusVA760 <tonio4340406@gmail.com>
2024-05-07 00:12:31 -0700

    PlayVideo layout

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -8,10 +8,59 @@ import tumbnail5 from "../../assets/thumbnail5.png";
 import tumbnail6 from "../../assets/thumbnail6.png";
 import tumbnail7 from "../../assets/thumbnail7.png";
 import tumbnail8 from "../../assets/thumbnail8.png";
+import { Link } from "react-router-dom";
 
 const Feed = () => {
   return (
     <div className="feed">
+      <Link to={`video/20/4521`} className="card">
+        <img src={tumbnail1} alt="" />
+        <h2>Best channel to learn coding that help you be a web delveloper</h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </Link>
+      <div className="card">
+        <img src={tumbnail2} alt="" />
+        <h2>Best channel to learn coding that help you be a web delveloper</h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={tumbnail3} alt="" />
+        <h2>Best channel to learn coding that help you be a web delveloper</h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={tumbnail4} alt="" />
+        <h2>Best channel to learn coding that help you be a web delveloper</h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={tumbnail5} alt="" />
+        <h2>Best channel to learn coding that help you be a web delveloper</h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={tumbnail6} alt="" />
+        <h2>Best channel to learn coding that help you be a web delveloper</h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={tumbnail7} alt="" />
+        <h2>Best channel to learn coding that help you be a web delveloper</h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+      <div className="card">
+        <img src={tumbnail8} alt="" />
+        <h2>Best channel to learn coding that help you be a web delveloper</h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
       <div className="card">
         <img src={tumbnail1} alt="" />
         <h2>Best channel to learn coding that help you be a web delveloper</h2>
